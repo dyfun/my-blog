441707166f7268e0e2a00b0a71440f65811c899a
Tayfun Güler <gulertayfun@outlook.com>
2022-01-25 13:03:59 +0300

    hero

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -3,7 +3,18 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="px-auto bg-hero-bg py-20">
-      <div className="container w-8/12 mx-auto">Hero</div>
+      <div className="container w-8/12 mx-auto">
+        <div className="text-center">
+          <div className="text-3xl">
+            <b className="font-semibold">Hello, </b>
+            I am Tayfun. <br />
+          </div>
+          <p className="block pt-4">
+            Freelance Web App Developer and Electric - Electronic Engineering
+            student
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
